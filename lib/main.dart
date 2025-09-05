898973d6cc08f6b6614c075a58c9afde5fa7acec
Yunitrish <yunthomas006@gmail.com>
2025-09-05 17:32:25 +0800

    fix: 更新 Supabase 服務的 URL，修正連接問題

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'http://coselig.com:8000',
+    url: 'https://coselig.com',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE',
   );
- 
 
   /*
 https://qikzlcnsyiihftudbmkp.supabase.co
