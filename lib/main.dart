3e15383eddfe23830c73c9090bdc2eb6d56a4da6
光悅server <coseligtest@gmail.com>
2025-09-05 16:33:38 +0800

    fix: 更新 Supabase 服務的 URL，修正連接問題

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'http://supabase.coselig.com:8000',
+    url: 'http://coselig.com:8000',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE',
   );
